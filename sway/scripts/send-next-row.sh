bc183113993b3d86abd7d27366acb8e84584b84f
First Last <colemanschmanke@gmail.com>
2025-11-09 13:26:53 -0600

    update 1

--- sway/scripts/send-next-row.sh
+++ sway/scripts/send-next-row.sh
@@ -2,6 +2,6 @@
 workspace=$(swaymsg -t get_workspaces | grep -B3 '"focused": true,' | head -n1 | awk '{print $2}' | tr -d ',')
 column=${workspace:0:1}
 row=${workspace:1}
-new=$((row + 1)); [ $new -gt 99 ] && new=99
+new_row=$((row + 1)); [ $new_row -gt 99 ] && new=99
 new_name=$column$new_row
 swaymsg move container to workspace $new_name
